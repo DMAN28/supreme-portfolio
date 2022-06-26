4064e40d54692659431422536e4d3bafe613f13b
David Manriquez <dmanriqu@my.chemeketa.edu>
2022-06-26 10:35:48 -0700

    added styling to the button

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,16 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+   <LeftSection>
+    <SectionTitle main center>
+      Portfolio<br/>
+      Personal Portfolio
+    </SectionTitle>
+    <SectionText>Hello my name is David and I'm a Fullstack web developer with an extensive experience within the medical field using and interacting with Electronic health records and Lab information systems. Successfully helped restructure clinic wide workflow to improve communication  by implementing Agile principles. I’m looking to leverage my ability to work in a high paced attention to detail oriented environments to help build great web applications.</SectionText>
+    <Button onClick={()=> window.location = 'https://www.google.com'}>Learn more</Button>
+   </LeftSection>
+  </Section>
 );
 
 export default Hero;
